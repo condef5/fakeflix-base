c74ce32f3025d4fd840039efc5dc518ca9ec1d3b
condef5 <davis.con.fab@gmail.com>
2019-04-23 16:36:24 -0500

    Add authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-
+    post 'authenticate', to: 'authentication#authenticate'
     resources :movies, only: [:index, :show] do
       patch "playback", on: :member
       patch "rating", on: :member
