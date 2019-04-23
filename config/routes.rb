ee6b23f822120df6b3deac37b233131e538827c9
condef5 <davis.con.fab@gmail.com>
2019-04-23 16:54:09 -0500

    Add profile user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     post 'authenticate', to: 'authentication#authenticate'
+    get 'profile', to: 'profile#index'
     resources :movies, only: [:index, :show] do
       patch "playback", on: :member
       patch "rating", on: :member
